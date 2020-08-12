0c51765866c916db9cf5979bf557e201715b0136
Nicolai Sehrt <nicolai.sehrt@mni.thm.de>
2020-08-13 01:56:29 +0200

    better timing

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
 extern crate minifb;
+extern crate spin_sleep;
 
 use minifb::{Key, Window, WindowOptions};
 use std::env;
+use std::time::{Duration, Instant};
 
 mod chip8;
 use chip8::Chip8;
 
-const WIDTH: usize = 640;
-const HEIGHT: usize = 480;
+const WIDTH: usize = 600;
+const HEIGHT: usize = 350;
 
 fn main() {
 
@@ -33,46 +35,23 @@ fn main() {
         panic!("{}", e);
     });
 
-    // Limit to max ~60 fps update rate
+    //window.limit_update_rate(Some(std::time::Duration::from_micros(16600)));
     window.limit_update_rate(None);
-
 
     //init
     let mut rust8 = Chip8::new();
     rust8.init();
     rust8.load_rom(rom_path);
 
+    let mut _instructions_processed: u64 = 0;
+
+    let mut start_time = Instant::now();
 
     while window.is_open() && !window.is_key_down(Key::Escape) {
 
         //emulator loop
         rust8.step();
-    
-        //update graphic output
-        if rust8.is_draw_flag() {
-            for (count, i) in buffer.iter_mut().enumerate() {
-
-                let x_coord = (count % WIDTH) as f64 / WIDTH as f64 * 64.0;
-                let y_coord = (count / WIDTH) as f64 / HEIGHT as f64 * 32.0;
-
-                if rust8.get_vram(x_coord as usize, y_coord as usize) {
-                    *i = 0;
-                }else{
-                    *i = std::u32::MAX;
-                }
-
-                
-            }   
-                 
-            rust8.reset_draw_flag();
-
-            window
-            .update_with_buffer(&buffer, WIDTH, HEIGHT)
-            .unwrap();
-        }else {
-            window.update();
-        }
-
+        _instructions_processed += 1;
 
         //update the keypad
         rust8.keypad[0x1] = window.is_key_down(Key::Key1) as u16;
@@ -91,6 +70,41 @@ fn main() {
         rust8.keypad[0x0] = window.is_key_down(Key::X) as u16;
         rust8.keypad[0xB] = window.is_key_down(Key::C) as u16;
         rust8.keypad[0xF] = window.is_key_down(Key::V) as u16;
+
+        //update graphic output
+        if rust8.is_draw_flag() {
+            for (count, i) in buffer.iter_mut().enumerate() {
+
+                let x_coord = (count % WIDTH) as f64 / WIDTH as f64 * 64.0;
+                let y_coord = (count / WIDTH) as f64 / HEIGHT as f64 * 32.0;
+
+                if rust8.get_vram(x_coord as usize, y_coord as usize) {
+                    *i = 0;
+                }else{
+                    *i = std::u32::MAX;
+                }
+
+                
+            }   
+                
+            rust8.reset_draw_flag();
+
+            window
+            .update_with_buffer(&buffer, WIDTH, HEIGHT)
+            .unwrap();
+
+            //delay
+            let delta = Instant::now().checked_duration_since(start_time).unwrap();
+
+            if delta < Duration::from_millis(16) {
+                let sleep_time = Duration::from_millis(16) - delta;
+                spin_sleep::sleep(sleep_time);
+            }
+
+            start_time = Instant::now();
+        }else {
+            window.update();
+        }
 
     }
 }
